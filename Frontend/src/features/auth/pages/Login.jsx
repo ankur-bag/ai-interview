67ec2e9da075740c0252cc4efad3758d22cf1b2d
Ankur Bag <ankurbag700@gmail.com>
2026-03-18 23:50:36 +0530

    frontend auth updated

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../auth.form.scss'
 import { Navigate,Link } from 'react-router'
+import { useAuth } from '../hooks/useAuth'
 const Login = () => {
+    const {loading,handleLogin} = useAuth()
+
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        handleLogin({email,password})
+    }
 
         const handle = (e)=>{
             e.preventDefault()
         }
 
 
+        if(loading){
+            return (
+                <main><h1>Loading....</h1></main>
+            )
+        }
+
   return (
     <main>
         <div className='form-container'>
             <h1>Login</h1>
-            <form>
+            <form  onSubmit={handleSubmit}    >
 
             <div className="input-group">
                 <label htmlFor='email'>Email</label>
-                <input type = "email" id="email" name='email' placeholder='Enter email'  />
+                <input
+                onChange={(e)=>{setEmail(e.target.value)}}
+                type = "email" id="email" name='email' placeholder='Enter email'  />
             </div>
             <div className="input-group">
                 <label htmlFor='email'>Password</label>
-                <input type = "email" id="email" name='email' placeholder='Enter password'  />
+                <input 
+                onChange={(e)=>{setPassword(e.target.value)}}
+                type = "email" id="email" name='email' placeholder='Enter password'  />
             </div>
 
             <button className='button primary-button ' onClick={handle}>Login</button>
